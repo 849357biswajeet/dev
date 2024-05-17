e04f44793be978e9c104f2ec627b434ca9ec7fc7
biswa <jroy5045@gmail.com>
2024-05-17 16:44:48 +0530

    fgh

---
+++ src/main/java/com/example/biswa/BiswaApplication.java
@@ -0,0 +1,13 @@
+package com.example.biswa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiswaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiswaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biswa/BiswaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biswa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiswaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
